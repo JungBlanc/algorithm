66b6a22033f226d39f9ac6cfa7950c6fd1d30f55
jungtaehyun <hoxi777@github.com>
2019-10-23 21:31:58 +0900

    study quicksort and mergesort

--- Sort/quicksort.py
+++ Sort/quicksort.py
@@ -1,11 +1,12 @@
 # 퀵 정렬
 # 분활 정렬이라고도 부름 (N*logN))
 
+
 def quick_sorted(arr):
     if len(arr) > 1:
-        pivot = arr[len(arr)-1]
+        pivot = arr[0]
         left, mid, right = [], [], []
-        for i in range(len(arr)-1):
+        for i in range(1, len(arr)):
             if arr[i] < pivot:
                 left.append(arr[i])
             elif arr[i] > pivot:
@@ -16,8 +17,7 @@ def quick_sorted(arr):
         return quick_sorted(left) + mid + quick_sorted(right)
     else:
         return arr
-        
-arr = [3, 5, 1, 2, 9, 6, 4, 7, 5]
+
+
+arr = [3, 5, 1, 2, 9, 6, 4, 7]
 print(quick_sorted(arr))
-
-
