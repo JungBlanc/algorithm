8e1b676094d31e57454da50aae7fc0bdc2a99ccf
JungTaeHyun <jtho1015@gmail.com>
2020-03-16 14:08:06 +0900

    :up: update: review bfs

--- bfs/bfs.py
+++ bfs/bfs.py
@@ -16,19 +16,36 @@ graph = {
 }
 
 
+# def bfs(graph, start_node):
+#     visit = list()
+#     queue = list()
+
+#     queue.append(start_node)
+
+#     while queue:
+#         node = queue.pop(0)
+#         if node not in visit:
+#             visit.append(node)
+#             queue.extend(graph[node])
+
+#     return visit
+
+
+# print(bfs(graph, 'A'))
+
+
 def bfs(graph, start_node):
-    visit = list()
+    visited = list()
     queue = list()
 
     queue.append(start_node)
-
     while queue:
         node = queue.pop(0)
-        if node not in visit:
-            visit.append(node)
+        if node not in visited:
+            visited.extend(node)
             queue.extend(graph[node])
 
-    return visit
+    return visited
 
 
 print(bfs(graph, 'A'))
